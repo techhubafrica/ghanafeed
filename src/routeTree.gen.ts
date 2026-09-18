fe5d6d9d819d838efd5e36347c8ab360ec7a63b0
kennan675 <kebowen675@gmail.com>
2026-09-18 11:51:34 +0300

    chore: update .gitignore and regenerate routeTree

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -75,12 +75,7 @@ export interface FileRoutesById {
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths:
-    | '/'
-    | '/privacy'
-    | '/search'
-    | '/article/$slug'
-    | '/c/$slug'
-    | '/t/$slug'
+    '/' | '/privacy' | '/search' | '/article/$slug' | '/c/$slug' | '/t/$slug'
   fileRoutesByTo: FileRoutesByTo
   to: '/' | '/privacy' | '/search' | '/article/$slug' | '/c/$slug' | '/t/$slug'
   id:
